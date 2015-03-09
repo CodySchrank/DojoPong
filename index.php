13abff8e09a3f33400573545e5f569a04d14067f
Cody Schrank <cody.schrank@gmail.com>
2015-03-09 13:55:36 -0700

    Updated Index

---
+++ index.php
@@ -0,0 +1,18 @@
+<?
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Dojo Pong</title>
+</head>
+<body>
+	<h1>Welcome To Dojo Pong</h1>
+	<h3>Track your stats</h3>
+	<form action="process.php" method="post">
+		<label>Your Name:</label>
+		<input>
+	</form>
+</body>
+</html>
